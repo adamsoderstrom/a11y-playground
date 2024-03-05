a731f23bb73e76463e261a6cdc86b0bd0ca0d856
adamsoderstrom <adamsoderstrom@me.com>
2024-03-05 15:38:09 +0100

    semantics/_3

--- apps/web/src/semantics/_3.tsx
+++ apps/web/src/semantics/_3.tsx
@@ -72,7 +72,71 @@ const _3 = () => {
       </div>
 
       <div className="max-w-2xl flex flex-col gap-4">
-        {/** Your solution goes here. */}
+        <h1 className="text-4xl mb-4">Checkout</h1>
+
+        <form
+          onSubmit={(event) => event?.preventDefault()}
+          className="grid grid-cols-2 gap-16"
+        >
+          <fieldset className="col-span-1 flex flex-col gap-2">
+            <legend className="font-bold mb-2">Personal Information</legend>
+
+            <div>
+              <label htmlFor="firstName">First Name:</label>
+              <input name="firstName" autoComplete="given-name" />
+            </div>
+
+            <div>
+              <label htmlFor="lastName">Last Name:</label>
+              <input name="lastName" autoComplete="family-name" />
+            </div>
+          </fieldset>
+
+          <fieldset className="col-span-1 flex flex-col gap-2">
+            <legend className="font-bold mb-2">Payment Information</legend>
+            <div>
+              <label htmlFor="country">Country:</label>
+              <input name="country" autoComplete="country" />
+            </div>
+
+            <div>
+              <label htmlFor="state">State:</label>
+
+              <input name="state" autoComplete="address-level1" />
+            </div>
+
+            <div>
+              <label htmlFor="address">Address:</label>
+              <input name="address" autoComplete="street-address" />
+            </div>
+
+            <div>
+              <label htmlFor="cardNumber">Card Number</label>
+              <input name="cardNumber" autoComplete="cc-number" />
+            </div>
+
+            <div>
+              <label htmlFor="cvv">CVV</label>
+              <input name="cvv" autoComplete="cc-csc" />
+            </div>
+          </fieldset>
+
+          <div className="col-span-2">
+            <div className="my-12 bg-white h-px w-full" />
+
+            <div className="flex flex-col gap-4 items-center">
+              <button type="submit" className="bg-yellow-600 text-white py-2 px-4 w-full max-w-xs">
+                Payment
+              </button>
+
+              <p className="max-w-xs text-xs text-gray-400">
+                By pressing "Payment", you certify that your details are
+                correct. No refunds will be issued after the payment
+                confirmation view is displayed.
+              </p>
+            </div>
+          </div>
+        </form>
       </div>
     </View>
   );
